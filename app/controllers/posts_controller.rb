4d6ae6b7846e9b95270d02bbb3f7430b5afffa03
joshsrai <joshsrai@gmail.com>
2021-02-17 11:58:24 -0500

    added PostSerializer to the index and create actions, added a destroy action in the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
     
     def index
-        render json: Post.all
+        render json: Post.all.map {|post| PostSerializer.new(post)}
     end
 	    
 	def create
         post = Post.new(post_params)
         if post.save
-	        render json: post
+	        render json: PostSerializer.new(post)
 	     end	
 	end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+    end
 	
 	private
     
