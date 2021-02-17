6110d559134f8795edbb98f92fea988555d22443
joshsrai <joshsrai@gmail.com>
2021-02-17 10:43:39 -0500

    added create and private comment params to the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+    def index
+        render json: Comment.all
+    end
+
+    def create
+        byebug
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            render json:comment
+        else 
+            byebug
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
 end
