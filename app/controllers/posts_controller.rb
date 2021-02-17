d8edeacfac572eb59f5615138b489aee8f89440d
joshsrai <joshsrai@gmail.com>
2021-02-17 08:58:01 -0500

    added index, create and private post params methods to the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+    
+    def index
+        render json: Post.all
+    end
+	    
+	def create
+        post = Post.new(post_params)
+        if post.save
+	        render json: post
+	     end	
+	end
+	
+	private
+    
+    def post_params
+	    params.require(:post).permit(:content)
+	end
+
 end
