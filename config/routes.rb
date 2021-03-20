6c37a0f81159b2e11bd2fe858e613dbf7ce03b5f
joshsrai <joshsrai@gmail.com>
2021-03-20 14:08:37 -0400

    added get-post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
+  get "/get/post", to: "posts#getOne"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
